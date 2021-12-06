052d62adae748a234aae14fc0bb89b8ea5e09e3b
Vincent Roth <vincent@placeme.io>
2021-12-06 16:48:36 +0100

    Pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AnimalService } from './api/animal.service';
 import { MailToDirective } from './mail-to.directive';
+import { FormatPhonePipe } from './format-phone.pipe';
 
 @NgModule({
-  declarations: [MailToDirective],
-  exports: [MailToDirective],
+  declarations: [MailToDirective, FormatPhonePipe],
+  exports: [MailToDirective, FormatPhonePipe],
   providers: [AnimalService],
 })
 export class SharedModule {}
