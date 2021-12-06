2fe6ca0c5c7c0c0e54593ddc21d21cd0bd9a36d4
Vincent Roth <vincent@placeme.io>
2021-12-06 15:55:27 +0100

    Directive structurelle (ngFor)

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
 
 @NgModule({
-  declarations: [AnimalDetailComponent],
+  declarations: [AnimalDetailComponent, AnimalListComponent],
   imports: [CommonModule, SharedModule],
-  exports: [AnimalDetailComponent],
+  exports: [AnimalDetailComponent, AnimalListComponent],
 })
 export class AnimalModule {}
--- src/app/shared/api/animal.service.ts
+++ src/app/shared/api/animal.service.ts
@@ -5,6 +5,35 @@ import { Animal } from './animal';
 export class AnimalService {
   constructor() {}
 
+  getAll(): Animal[] {
+    return [
+      {
+        name: 'Idéfix 1 du service',
+        species: 'dog',
+        veterinarian: "Panoramix D'Armorique",
+        comment: 'Développe une allergie à la potion magique.',
+        email: 'test1@gmail.com',
+        phoneNumber: '06.12.34.56.78',
+      },
+      {
+        name: 'Idéfix 2 du service',
+        species: 'dog',
+        veterinarian: "Panoramix D'Armorique",
+        comment: 'Développe une allergie à la potion magique.',
+        email: 'test1@gmail.com',
+        phoneNumber: '06.12.34.56.78',
+      },
+      {
+        name: 'Idéfix 3 du service',
+        species: 'dog',
+        veterinarian: "Panoramix D'Armorique",
+        comment: 'Développe une allergie à la potion magique.',
+        email: 'test1@gmail.com',
+        phoneNumber: '06.12.34.56.78',
+      },
+    ];
+  }
+
   get(): Animal {
     return {
       name: 'Idéfix du service',
