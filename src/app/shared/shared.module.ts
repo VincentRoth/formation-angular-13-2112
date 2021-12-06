b53225dac43c7ea30bc88e77a589991f6b37d06f
Vincent Roth <vincent@placeme.io>
2021-12-06 16:08:05 +0100

    Directive d'attribut

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AnimalService } from './api/animal.service';
+import { MailToDirective } from './mail-to.directive';
 
 @NgModule({
+  declarations: [MailToDirective],
+  exports: [MailToDirective],
   providers: [AnimalService],
 })
 export class SharedModule {}
