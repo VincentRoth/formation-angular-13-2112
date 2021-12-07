5be0a23276add1701173124fe1ebff3b0d38f433
Vincent Roth <vincent@placeme.io>
2021-12-07 16:28:42 +0100

    Observable et désouscription

--- src/app/animal/animal-detail/animal-detail.component.ts
+++ src/app/animal/animal-detail/animal-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Animal } from '../../shared/api/animal';
 import { AnimalService } from '../../shared/api/animal.service';
 
@@ -8,8 +9,9 @@ import { AnimalService } from '../../shared/api/animal.service';
   templateUrl: './animal-detail.component.html',
   styleUrls: ['./animal-detail.component.scss'],
 })
-export class AnimalDetailComponent implements OnInit {
+export class AnimalDetailComponent implements OnInit, OnDestroy {
   animal: Animal;
+  private subscription: Subscription;
 
   constructor(
     private animalService: AnimalService,
@@ -17,10 +19,18 @@ export class AnimalDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.subscription = this.activatedRoute.paramMap.subscribe(
+      (paramMap: ParamMap) => {
+        const id = paramMap.get('id');
 
-    this.animalService.get(Number(id)).subscribe((data) => {
-      this.animal = data;
-    });
+        this.animalService.get(Number(id)).subscribe((data) => {
+          this.animal = data;
+        });
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
