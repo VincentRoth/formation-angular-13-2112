b656e56c210b0a07e1f65d63dec6ba070e915154
Vincent Roth <vincent@placeme.io>
2021-12-08 15:29:58 +0100

    Validation du formulaire piloté par le template

--- src/app/animal/animal-form/animal-form.component.ts
+++ src/app/animal/animal-form/animal-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Animal } from '../../shared/api/animal';
 import { AnimalService } from '../../shared/api/animal.service';
@@ -10,6 +11,7 @@ import { AnimalService } from '../../shared/api/animal.service';
 })
 export class AnimalFormComponent implements OnInit {
   model: Animal;
+  @ViewChild('animalNgForm') ngForm: NgForm;
 
   constructor(
     private router: Router,
@@ -36,14 +38,16 @@ export class AnimalFormComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const onSave = () => {
-      this.router.navigate(['/animals']);
-    };
+    if (this.ngForm.form.valid) {
+      const onSave = () => {
+        this.router.navigate(['/animals']);
+      };
 
-    if (this.model.id) {
-      this.animalService.update(this.model).subscribe(onSave);
-    } else {
-      this.animalService.create(this.model).subscribe(onSave);
+      if (this.model.id) {
+        this.animalService.update(this.model).subscribe(onSave);
+      } else {
+        this.animalService.create(this.model).subscribe(onSave);
+      }
     }
   }
 }
